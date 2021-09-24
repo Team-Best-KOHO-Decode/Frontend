ceb4480e0fe6a05b99b10c89c6c4bda594f11157
Anthony Reyes <anthonyreyesf@gmail.com>
2021-09-23 20:38:23 -0400

    created dashboard page + button component

---
+++ src/Components/Button/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'; 
+import "./styles.css";
+
+interface Props {
+  text: string;
+}
+
+function Button(props: Props) {
+  const {
+    text,
+  } = props;
+  return (
+    <button className="button">
+      {text}
+    </button>
+  );
+}
+
+export default Button;
---
+++ src/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from "../Components/Button";
+
+function DashboardPage() {
+  return (
+    <div>
+      <Button text="Hello"/>
+    </div>
+  );
+}
+
+export default DashboardPage;
