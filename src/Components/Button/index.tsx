d0a0f6eeb5a752db7c1dd02661582856b54d9088
Cuong Tran <cuong@koho.ca>
2021-09-24 16:20:58 -0400

    make onClick optional

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -4,11 +4,11 @@ import "./styles.css";
 interface Props {
   text: string;
   color: "primary" | "secondary" | "tertiary";
-  onClick: () => void;
+  onClick?: () => void;
 }
 
 function Button(props: Props) {
-  const { text, color, onClick } = props;
+  const { text, color, onClick = () => {} } = props;
 
   return (
     <button onClick={() => onClick()} className={"button " + color}>
