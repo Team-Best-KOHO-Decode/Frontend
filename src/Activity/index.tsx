0db82edeea46b5c12539df8bf25f2ece0a4f80d7
Anthony Reyes <anthonyreyesf@gmail.com>
2021-09-24 12:22:40 -0400

    created activity page

---
+++ src/Activity/index.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Button from "../Components/Button";
+import Activity from "../Components/Activity";
+import ActivityType from "../Components/ActivityType";
+
+import "./styles.css";
+
+function ActivityPage() {
+  const activityTypes = ["Food", "Entertainment", "Sports"];
+  const activities = [
+    {
+      name: "Mario's",
+      type: "Italian",
+      price: "$$",
+    },
+    {
+      name: "Subway",
+      type: "Fast Food",
+      price: "$",
+    },
+    {
+      name: "Lee Restaurant",
+      type: "Bougie",
+      price: "$$$",
+    },
+  ];
+
+  return (
+    <div className="root">
+      <div className="info">
+        <p>Recommended Activities</p>
+        <p>Based on your Group Budget of 15-30$ and location</p>
+      </div>
+      <div className="activitesList">
+        {activityTypes.map((type) => (
+          <ActivityType type={type} />
+        ))}
+      </div>
+      {activities.map((activity) => (
+        <Activity
+          name={activity.name}
+          type={activity.type}
+          price={activity.price}
+        />
+      ))}
+      <Button text="Hello" />
+    </div>
+  );
+}
+
+export default ActivityPage;
